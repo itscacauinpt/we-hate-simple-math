9eb28c4c56bd1d09e26e46bb96f8fd0b47599742
cacau <anaclaudia.sza@outlook.com>
2022-11-28 19:06:39 -0300

    feat: add context provider for score

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Menu from './Pages/Menu';
 import Game from './Pages/Game';
+import Score from './Pages/Score';
 import Credits from './Pages/Credits';
 import MoreAbout from './Pages/MoreAbout';
 
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route exact path="/menu" element={ <Menu /> } />
       <Route exact path="/game" element={ <Game /> } />
+      <Route exact path="/game-score" element={ <Score /> } />
       <Route exact path="/credits" element={ <Credits /> } />
       <Route exact path="/more-about-math-anxiety" element={ <MoreAbout /> } />
 
--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "../Components/Button";
@@ -9,51 +9,84 @@ import backButton from "../img/back-button.svg"
 
 import { startGame } from "../utils/getExpressions";
 
+import GameContext from "../Context/";
+
 export default function Game() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
+  const [symbol, setSymbol] = useState();
+  const [userAnswer, setUserAnswer] = useState();
   const [expression, setExpression] = useState({});
-  // const [results, setResults] = useContext(); // isso aqui também aqui vai estar no context
+
+  const { functions: { setResult, setAnswers } } = useContext(GameContext);
 
   const backMenu = () => navigate("/menu");
 
-  const submitResult = () => console.log('deu, setResults, array de previous + current');
+  const handleChanger = ({value}) => {
+    if (value <= -1) value = 0;
+
+    setUserAnswer(value);
+  }
+
+  const getExpression = () => {
+    const expr = startGame();
+
+    setExpression(expr);
+    setSymbol(expr.symbol);
+    setResult(prev => [...prev, expr.result]);
+  }
+
+  const submitResult = () => {
+    getExpression();
+
+    setUserAnswer(0);
+    setAnswers(prev => [...prev, userAnswer]);
+  }
 
   useEffect(() => {
-    const expr = startGame();
-    setExpression(expr);
+    getExpression();
 
+    setLoading(true);
   }, []);
 
   return(
-    <>
-      <Button
-        title={ 'back to menu button' }
-        image={ backButton }
-        functionGame={ backMenu }
-      />
-      <section>
-      </section>
-      <section>
+    <section>
+    {
+      loading ?
         <div>
-          <p>{expression.prod}</p>
-          <p>{expression.secondProd}</p>
-          <Symbol svgTitle={ expression.symbol } />
+          <Button
+            title={'back to menu button'}
+            image={backButton}
+            functionGame={backMenu}
+          />
+
+          <div class="border-2">
+            <div>
+              <p>{expression.prod}</p>
+              <p>{expression.secondProd}</p>
+              <Symbol svgTitle={symbol} />
+            </div>
+            <div>
+              <input
+                type="number"
+                value={userAnswer}
+                onChange={({target}) => handleChanger(target)}
+                placeholder="0"
+              />
+              <SimpleButton
+                title={"submit"}
+                functionGame={submitResult}
+                classStyle={"border-2 rounded-full md:bg-gray-d9 hover:border-slate-400"}
+              />
+            </div>
+          </div>
         </div>
-      </section>
-      <section>
+      :
         <div>
-          <input
-              type="number"
-              placeholder="54...?"
-          />
-          <SimpleButton
-            title={ "submit" }
-            functionGame={ submitResult }
-            classStyle={ "border-2 rounded-full md:bg-gray-d9 hover:border-slate-400" }
-          />
+          <p>Loading...</p>
         </div>
-      </section>
-    </>
+    }
+    </section>
   )
 }
